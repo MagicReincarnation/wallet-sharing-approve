11a28e511a05451f68c5145c8637dcd50f349590
MagicReincarnation <lunapgr01@gmail.com>
2025-12-26 10:57:37 +0700

    update go

---
+++ cmd/commands.go
@@ -0,0 +1,208 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"time"
+
+	dbm "github.com/cosmos/cosmos-db"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"cosmossdk.io/log"
+	confixcmd "cosmossdk.io/tools/confix/cmd"
+	"github.com/paxi-web3/paxi/app"
+
+	wasmcli "github.com/CosmWasm/wasmd/x/wasm/client/cli"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/debug"
+	"github.com/cosmos/cosmos-sdk/client/keys"
+	"github.com/cosmos/cosmos-sdk/client/pruning"
+	"github.com/cosmos/cosmos-sdk/client/rpc"
+	"github.com/cosmos/cosmos-sdk/client/snapshot"
+	"github.com/cosmos/cosmos-sdk/server"
+	servertypes "github.com/cosmos/cosmos-sdk/server/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
+	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
+	version "github.com/paxi-web3/paxi"
+	"github.com/paxi-web3/paxi/utils"
+	custommintcli "github.com/paxi-web3/paxi/x/custommint/client"
+	paxicli "github.com/paxi-web3/paxi/x/paxi/client"
+	swapcli "github.com/paxi-web3/paxi/x/swap/client"
+)
+
+func initRootCmd(
+	rootCmd *cobra.Command,
+	txConfig client.TxConfig,
+	basicManager module.BasicManager,
+	a *app.PaxiApp,
+) {
+	rootCmd.AddCommand(
+		InitCmd(a, app.DefaultNodeHome),
+		debug.Cmd(),
+		confixcmd.ConfigCommand(),
+		pruning.Cmd(newApp, app.DefaultNodeHome),
+		snapshot.Cmd(newApp),
+	)
+
+	server.AddCommandsWithStartCmdOptions(rootCmd, app.DefaultNodeHome, newAppWithWasm, appExport, server.StartCmdOptions{
+		AddFlags: func(startCmd *cobra.Command) {
+			startCmd.PreRunE = func(cmd *cobra.Command, args []string) error {
+				// Clean wasm cache
+				utils.CleanOldWasmCache(app.DefaultNodeHome, 1*24*time.Hour)
+				return nil
+			}
+		},
+	})
+
+	// add keybase, auxiliary RPC, query, genesis, and tx child commands
+	rootCmd.AddCommand(
+		versionCmd(),
+		server.StatusCommand(),
+		genesisCommand(txConfig, basicManager),
+		queryCommand(),
+		txCommand(),
+		keys.Commands(),
+	)
+}
+
+// genesisCommand builds genesis-related `paxid genesis` command. Users may provide application specific commands as a parameter
+func genesisCommand(txConfig client.TxConfig, basicManager module.BasicManager, cmds ...*cobra.Command) *cobra.Command {
+	cmd := genutilcli.Commands(txConfig, basicManager, app.DefaultNodeHome)
+
+	for _, subCmd := range cmds {
+		cmd.AddCommand(subCmd)
+	}
+	return cmd
+}
+
+func queryCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        "query",
+		Aliases:                    []string{"q"},
+		Short:                      "Querying subcommands",
+		DisableFlagParsing:         false,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(
+		rpc.WaitTxCmd(),
+		server.QueryBlockCmd(),
+		authcmd.QueryTxsByEventsCmd(),
+		server.QueryBlocksCmd(),
+		authcmd.QueryTxCmd(),
+		server.QueryBlockResultsCmd(),
+		wasmcli.GetQueryCmd(),
+		paxicli.GetQueryCmd(),
+		swapcli.GetQueryCmd(),
+		custommintcli.GetQueryCmd(),
+	)
+
+	return cmd
+}
+
+func txCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        "tx",
+		Short:                      "Transactions subcommands",
+		DisableFlagParsing:         false,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(
+		authcmd.GetSignCommand(),
+		authcmd.GetSignBatchCommand(),
+		authcmd.GetMultiSignCommand(),
+		authcmd.GetMultiSignBatchCmd(),
+		authcmd.GetValidateSignaturesCommand(),
+		authcmd.GetBroadcastCommand(),
+		authcmd.GetEncodeCommand(),
+		authcmd.GetDecodeCommand(),
+		authcmd.GetSimulateCmd(),
+		paxicli.GetTxCmd(),
+		swapcli.GetTxCmd(),
+		wasmcli.GetTxCmd(),
+	)
+
+	return cmd
+}
+
+// newApp creates the application
+func newApp(
+	logger log.Logger,
+	db dbm.DB,
+	traceStore io.Writer,
+	appOpts servertypes.AppOptions,
+) servertypes.Application {
+	baseappOptions := server.DefaultBaseappOptions(appOpts)
+	return app.NewPaxiApp(
+		logger, db, traceStore, true,
+		appOpts,
+		false,
+		baseappOptions...,
+	)
+}
+
+// newApp creates the application with wasm enabled
+func newAppWithWasm(
+	logger log.Logger,
+	db dbm.DB,
+	traceStore io.Writer,
+	appOpts servertypes.AppOptions,
+) servertypes.Application {
+	baseappOptions := server.DefaultBaseappOptions(appOpts)
+	return app.NewPaxiApp(
+		logger, db, traceStore, true,
+		appOpts,
+		true, // enable Wasm
+		baseappOptions...,
+	)
+}
+
+// appExport creates a new paxiapp (optionally at a given height) and exports state.
+func appExport(
+	logger log.Logger,
+	db dbm.DB,
+	traceStore io.Writer,
+	height int64,
+	forZeroHeight bool,
+	jailAllowedAddrs []string,
+	appOpts servertypes.AppOptions,
+	modulesToExport []string,
+) (servertypes.ExportedApp, error) {
+	viperAppOpts, ok := appOpts.(*viper.Viper)
+	if !ok {
+		return servertypes.ExportedApp{}, errors.New("appOpts is not viper.Viper")
+	}
+
+	// overwrite the FlagInvCheckPeriod
+	viperAppOpts.Set(server.FlagInvCheckPeriod, 1)
+	appOpts = viperAppOpts
+
+	var paxiApp *app.PaxiApp
+	if height != -1 {
+		paxiApp = app.NewPaxiApp(logger, db, traceStore, false, appOpts, false)
+
+		if err := paxiApp.LoadHeight(height); err != nil {
+			return servertypes.ExportedApp{}, err
+		}
+	} else {
+		paxiApp = app.NewPaxiApp(logger, db, traceStore, true, appOpts, false)
+	}
+
+	return paxiApp.ExportAppStateAndValidators(forZeroHeight, jailAllowedAddrs, modulesToExport)
+}
+
+func versionCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Print the version of Paxi",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Version: %s\n", version.AppVersion)
+		},
+	}
+}
---
+++ cmd/paxid/main.go
@@ -0,0 +1,21 @@
+// Paxi Blockchain Main Entry
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/paxi-web3/paxi/app"
+	paxicmd "github.com/paxi-web3/paxi/cmd"
+
+	clientv2helpers "cosmossdk.io/client/v2/helpers"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+)
+
+func main() {
+	rootCmd := paxicmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, clientv2helpers.EnvPrefix, app.DefaultNodeHome); err != nil {
+		fmt.Fprintln(rootCmd.OutOrStderr(), err)
+		os.Exit(1)
+	}
+}
